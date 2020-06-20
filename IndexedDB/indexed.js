e9f64c0219ddc426201bcc6adbdc2b66248cb28d
vill <kevin.ricardo@ciencias.unam.mx>
2020-06-20 17:51:10 -0500

    Busqueda por año

--- IndexedDB/indexed.js
+++ IndexedDB/indexed.js
@@ -1,16 +1,16 @@
 function iniciar(){
-    cajadatos = document.getElementById('cajadatos');
-    var boton = document.getElementById('grabar');
-    boton.addEventListener('click', agregarobjeto, false);
+    cajadatos=document.getElementById('cajadatos');
+    var boton=document.getElementById('buscar');
+    boton.addEventListener('click', buscarobjetos, false);
     if('webkitIndexedDB' in window){
-	window.indexedDB = window.webkitIndexedDB;
-	window.IDBTransaction = window.webkitIDBTransaction;
-	window.IDBKeyRange = window.webkitIDBKeyRange;
-	window.IDBCursor = window.webkitIDBCursor;
+	window.indexedDB=window.webkitIndexedDB;
+	window.IDBTransaction=window.webkitIDBTransaction;
+	window.IDBKeyRange=window.webkitIDBKeyRange;
+	window.IDBCursor=window.webkitIDBCursor;
     }else if('mozIndexedDB' in window){
-	window.indexedDB = window.mozIndexedDB;
+	window.indexedDB=window.mozIndexedDB;
     }
-    var solicitud = indexedDB.open('mibase');
+    var solicitud=indexedDB.open('mibase');
     solicitud.addEventListener('error', errores, false);
     solicitud.addEventListener('success', crear, false);
 }
@@ -18,46 +18,33 @@ function errores(e){
     alert('Error: '+e.code+' '+e.message);
 }
 function crear(e){
-    bd = e.result || e.target.result;
-    if(bd.version == ''){
-	var solicitud = bd.setVersion('1.0');
+    bd=e.result || e.target.result;
+    if(bd.version==''){
+	var solicitud=bd.setVersion('1.0');
 	solicitud.addEventListener('error', errores, false);
 	solicitud.addEventListener('success', crearbd, false);
-    }else{
-	mostrar();
     }
 }
 function crearbd(){
-    var almacen = bd.createObjectStore('peliculas',{keyPath: 'id'});
-    almacen.createIndex('BuscarFecha', 'fecha',{unique: false});
+    var almacen=bd.createObjectStore('peliculas', {keyPath: 'id'});
+    almacen.createIndex('BuscarFecha', 'fecha', { unique: false });
 }
-function agregarobjeto(){
-    var clave = document.getElementById('clave').value;
-    var titulo = document.getElementById('texto').value;
-    var fecha = document.getElementById('fecha').value;
-    var transaccion = bd.transaction(['peliculas'],
-				     IDBTransaction.READ_WRITE);
-    var almacen = transaccion.objectStore('peliculas');
-    var solicitud = almacen.add({id: clave, nombre: titulo, fecha:
-				 fecha});
-    solicitud.addEventListener('success', mostrar, false);
-    document.getElementById('clave').value='';
-    document.getElementById('texto').value='';
-    document.getElementById('fecha').value='';
-}
-function mostrar(){
+function buscarobjetos(){
     cajadatos.innerHTML='';
-    var transaccion = bd.transaction(['peliculas']);
-    var almacen = transaccion.objectStore('peliculas');
-    var cursor = almacen.openCursor();
+    var buscar=document.getElementById('fecha').value;
+    var transaccion=bd.transaction(['peliculas']);
+    var almacen=transaccion.objectStore('peliculas');
+    var indice=almacen.index('BuscarFecha');
+    var rango=IDBKeyRange.only(buscar);
+    var cursor=indice.openCursor(rango);
     cursor.addEventListener('success', mostrarlista, false);
 }
 function mostrarlista(e){
-    var cursor = e.result || e.target.result;
+    var cursor=e.result || e.target.result;
     if(cursor){
-	cajadatos.innerHTML += '<div>'+cursor.value.id+' - '+
-	    cursor.value.nombre+' - '+cursor.value.fecha+'</div>';
+	cajadatos.innerHTML+='<div>'+cursor.value.id+' -
+'+cursor.value.nombre+' - '+cursor.value.fecha+'</div>';
 	cursor.continue();
     }
 }
-window.addEventListener('load',iniciar,false);
+window.addEventListener('load', iniciar, false);
