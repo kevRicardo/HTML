5c380c01e67f86c304692afb6efb0c8050e5372f
vill <kevin.ricardo@ciencias.unam.mx>
2020-06-19 19:48:24 -0500

    Agregando escuchas para eventos

--- estilos3/micodigo.js
+++ estilos3/micodigo.js
@@ -2,6 +2,17 @@ function mostraralerta(){
 	alert('hizo clic!');
 }
 function hacerclic(){
-	document.getElementsByTagName('p')[0].onclick=mostraralerta;
+	/*Se implementan formas de hacer clic en el primer p*/
+	/*document.getElementsByTagName('p')[0].onclick=mostraralerta;*/
+	/*document.querySelector("#principal p:first-child").onclick=mostraralerta;*/
+	/*var lista=document.querySelectorAll("#principal p");
+	lista[0].onclick=mostraralerta;*/
+	/*Se implementa para todos los p*/
+	/*var lista=document.querySelectorAll("#principal p");
+	for (var f = 0; f < lista.length; f++){
+		lista[f].onclick=mostraralerta;
+	}*/
+	var lista=document.getElementById('principal').querySelectorAll("p");
+	lista[0].onclick=mostraralerta;
 }
 window.onload=hacerclic;
